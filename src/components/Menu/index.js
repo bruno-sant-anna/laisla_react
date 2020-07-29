a7be1cf771dce1174fc4044c4de40f305fc6141d
bruno-sant-anna <bsvtelecom@gmail.com>
2020-07-29 07:44:17 +0100

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+//import ButtonLink from '../components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/home">
+                <img className="Logo" src={Logo} alt="LAISLA logo" />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo modelito
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
