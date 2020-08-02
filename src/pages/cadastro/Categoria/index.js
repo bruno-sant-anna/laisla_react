3e1e4d4ca5c5935be78e550394d123e65c334aaf
bruno-sant-anna <bsvtelecom@gmail.com>
2020-08-02 10:46:20 +0100

    create form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,13 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const valoresIniciais = {
+        modelito: '',
+        cliente: '',
+        descricao: '',
+        cor: ''
+    }
+
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave]: valor, // modelito: 'valor'
+        })
+    }
+
+    function handleChange(infosDoEvento) {
+        const { getAttribute, value } = infosDoEvento.target;
+        setValue(
+            getAttribute('name'),
+            value
+        );
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastrar Categoria</h1>
+            <h2>Cadastrar Ficha Tecnica: {values.modelito}</h2>
 
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    values
+                ]);
 
+                setValues(valoresIniciais)
+            }}>
+
+                <FormField 
+                    label="Nome da Peça"
+                    type="text"
+                    name="modelito"
+                    value={values.modelito}
+                    onChange={handleChange}
+                />
+                <FormField 
+                    label="Cliente"
+                    type="text"
+                    name="cliente"
+                    value={values.cliente}
+                    onChange={handleChange}
+                />
+                {/* <div>
+                    <label>
+                        Cliente:
+                        <input
+                            type="text"
+                            value={values.cliente}
+                            name="cliente"
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div> */}
+                <FormField 
+                    label="Descrição"
+                    type="text"
+                    name="descricao"
+                    value={values.descricao}
+                    onChange={handleChange}
+                />
+                {/* <div>
+                    <label>
+                        Descrição:
+                        <input
+                            type="text"
+                            value={values.descricao}
+                            name="descricao"
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div> */}
+                <FormField 
+                    label="Cor"
+                    type="color"
+                    name="cor"
+                    value={values.cor}
+                    onChange={handleChange}
+                />
+                {/* <div>
+                    <label>
+                        Cor:
+                        <input
+                            type="color"
+                            value={values.cor}
+                            name="cor"
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div> */}
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                            {categoria.modelito}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para home
